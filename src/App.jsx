4625832fef23d15a428d8365666b8b323cf0b64c
Ali Hamidzadeh <hamidzadeh.80.ali@gmail.com>
2026-04-30 23:19:20 +0330

    set up routes and add root path redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Dashboard from "./pages/Dashboard";
+import Products from "./pages/Products";
+import NotFoundPage from "./pages/NotFoundPage";
+
 function App() {
   return (
     <div>
-      <h1>BotoStar React.js</h1>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace/>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
     </div>
   );
 }
