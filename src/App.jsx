ff3d67149100e16ddcd1342bb00a00784a7d4fa1
Ali Hamidzadeh <hamidzadeh.80.ali@gmail.com>
2026-05-02 17:54:44 +0330

    add routes for Products, ProductDetail, Login, Register and AdminProducts

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import AdminProducts from "./pages/admin/AdminProducts";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import Dashboard from "./pages/Dashboard";
 import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
 import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace/>} />
+        <Route path="/" element={<Navigate to="/products" replace />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/admin/products" element={<AdminProducts />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
         <Route path="/*" element={<NotFoundPage />} />
       </Routes>
     </div>
